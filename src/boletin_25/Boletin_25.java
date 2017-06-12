a65eea8e40713e639be084a839e5bdec8e200bdd
artur <artur@192.168.1.108>
2017-06-12 11:50:09 +0200

    Boletin_25

---
+++ src/boletin_25/Boletin_25.java
@@ -0,0 +1,14 @@
+package boletin_25;
+
+public class Boletin_25{
+
+    public static void main(String[] args) {
+        
+        Ejercicio_4 v = new Ejercicio_4();
+        
+        v.setVisible(true);
+        
+        //aproveché el ejercicio 4 para ejecutar las ventanas de los ejercicios 1,2 y 3
+    }
+
+}
